4b931b30537e424649d05dde374a8c13aa81e595
ericmina83 <ericmina83@gmail.com>
2016-01-27 15:04:18 +0800

    install ver 2

--- install.php
+++ install.php
@@ -21,8 +21,9 @@
     delete_at TIMESTAMP,
     primary_mail BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (primary_mail) REFERENCES mail (id)
+    unique (username)
     );";
+  // -- ,FOREIGN KEY (primary_mail) REFERENCES mail (id)
 
     $dbh->query($sql);
 
@@ -31,9 +32,18 @@
     id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     email BIGINT NOT NULL,
+    delete_at TIMESTAMP,
     PRIMARY KEY (id)
     );";
 
     $dbh->query($sql);
 
 
+    $insert_member_stmt = $dbh->prepare("insert into member (username, password, create_at, update_at, delete_at, primary_mail) values (:username, :password, :create_at, :update_at, :delete_at, :primary_mail)");
+
+    if (($handle = fopen("member.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $insert_member_stmt->execute(array(":username" => $data[1], ":password" => $data[2], ":create_at" => $data[3], ":update_at" => $data[4], ":delete_at" => $data[5], ":primary_mail" => $data[6]));
+        }
+        fclose($handle);
+    }
